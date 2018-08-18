ae4503dc9f4161f02fc869a6b2385ac74b3b7bd3
hemulin <mr.hemulin@gmail.com>
2018-08-18 02:34:47 +0200

    landing and login WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,50 @@
-<template>
-  <div>
-    <nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/nuxt-ui/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28">
-        </a>
+<template lang='pug'>
+section.hero.is-info.is-fullheight
+  .hero-head
+    nav.navbar
+      .container
+        .navbar-brand
+          a.navbar-item(href='../') MVP UI
+            //- img(src='http://bulma.io/images/bulma-type-white.png', alt='Logo')
 
-        <div class="navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon"/> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-
-    </section>
-  </div>
+          span.navbar-burger.burger(data-target='navbarMenu')
+            span
+            span
+            span
+        #navbarMenu.navbar-menu
+          .navbar-end
+            //- span.navbar-item
+            //-   a.button.is-white.is-outlined(href='#')
+            //-     span.icon
+            //-       b-icon(icon='home')
+            //-     span Home
+            //- span.navbar-item
+            //-   a.button.is-white.is-outlined(href='#')
+            //-     span.icon
+            //-       b-icon(icon='superpowers')
+            //-     span Examples
+            //- span.navbar-item
+            //-   a.button.is-white.is-outlined(href='#')
+            //-     span.icon
+            //-       b-icon(icon='book')
+            //-     span Documentation
+            span.navbar-item
+              a.button.is-white.is-outlined(href='#')
+                span.icon
+                  b-icon(icon='account')
+                span Login
+  .container
+    nuxt
+    //- .container.has-text-centered
+    //-   .column.is-6.is-offset-3
+    //-     h1.title
+    //-     h2.subtitle
+    //-     .box
+    //-       .field.is-grouped
+    //-         p.control.is-expanded
+    //-           input.input(type='text', placeholder='Enter your email')
+    //-         p.control
+    //-           a.button.is-info
 </template>
 
 <script>
@@ -47,3 +59,23 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  .hero.is-info {
+    background: linear-gradient(
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+      ), url('~/img/lettuce_background.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  .hero .nav, .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+  .hero .subtitle {
+    padding: 3rem 0;
+    line-height: 1.5;
+  }
+</style>
